a1fc708e3149f651741a74f71da64d1db54431ab
buusra <busrakirsac@gmail.com>
2023-12-23 22:52:47 +0300

    update

--- src/test/java/get_request/Get07.java
+++ src/test/java/get_request/Get07.java
@@ -2,11 +2,14 @@ package get_request;
 
 import baseUrl.HerokuAppBaseUrl;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Get07 extends HerokuAppBaseUrl {
 
@@ -47,6 +50,7 @@ public class Get07 extends HerokuAppBaseUrl {
         response.prettyPrint();
 
         //Do assertions
+        //1.yol
         response.then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
@@ -57,5 +61,27 @@ public class Get07 extends HerokuAppBaseUrl {
                 .body("bookingdates.checkin", equalTo("2018-01-01"))
                 .body("bookingdates.checkout", equalTo("2019-01-01"))
                 .body("additionalneeds", equalTo("super bowls"));
+
+
+        //JsonPath Alistirmalari
+        JsonPath json = response.jsonPath();
+        json.getString("firstname");
+        int totatPrice = json.getInt("totalprice");
+        System.out.println("totalPrice : " +totatPrice);
+        json.getString("bookingdates.checkin");
+        json.getString("bookingdates.checkout");
+
+
+        //2.yol
+        assertEquals(200,response.statusCode());
+        assertTrue(response.contentType().contains("application/json"));
+        assertEquals("Josh",json.getString("firstname"));
+        assertEquals("Allen",json.getString("lastname"));
+        assertEquals(111,json.getInt("totalprice"));
+        assertTrue(json.getBoolean("depositpaid"));
+        assertEquals("2018-01-01",json.getString("bookingdates.checkin"));
+        assertEquals("2019-01-01",json.getString("bookingdates.checkout"));
+        assertEquals("super bowls",json.getString("additionalneeds"));
+
     }
 }
