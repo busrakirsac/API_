795ca5dd5cfadebd0bb7ec2a18789c51705e63ac
buusra <busrakirsac@gmail.com>
2023-12-24 18:10:27 +0300

    v2

--- src/test/java/post_request/Post02.java
+++ src/test/java/post_request/Post02.java
@@ -5,6 +5,9 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -67,6 +70,25 @@ public class Post02 extends JsonPlaceHolderBaseUrl {
     @Test
     public void post02Map() {
 
+        // Set the URL
+        spec.pathParam("first", "todos");
 
+        // Set the expected data (payLoad)
+        Map<String,Object> payLoad =new HashMap<>();
+        payLoad.put("userId",55);
+        payLoad.put("title","Tidy your room");
+        payLoad.put("completed",false);
+        System.out.println(payLoad);
+
+        //Send the request and get the response
+        Response response = given(spec).body(payLoad).when().post("{first}");
+        response.prettyPrint();
+
+        //Do assertions
+        Map<String,Object> actualData = response.as(HashMap.class);
+        assertEquals(201, response.statusCode());
+        assertEquals(payLoad.get("userId"),actualData.get("userId"));
+        assertEquals(payLoad.get("title"),actualData.get("title"));
+        assertEquals(payLoad.get("completed"),actualData.get("completed"));
     }
 }
