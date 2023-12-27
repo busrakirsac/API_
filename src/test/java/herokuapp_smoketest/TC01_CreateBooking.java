985fa11f6488c9c8a39999bc7b029368b12bea9a
buusra <busrakirsac@gmail.com>
2023-12-27 17:03:49 +0300

    v2

--- src/test/java/herokuapp_smoketest/TC01_CreateBooking.java
+++ src/test/java/herokuapp_smoketest/TC01_CreateBooking.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.assertEquals;
 
 public class TC01_CreateBooking extends HerokuAppBaseUrl {
 
+    public static int bookingId;
 
     /*
 
@@ -62,17 +63,18 @@ public class TC01_CreateBooking extends HerokuAppBaseUrl {
         BookingPojo payLoad = new BookingPojo("Jim", "Brown", 111, true, bookingDates, "Breakfast");
 
         Response response = given(spec).body(payLoad).when().post("{first}");
-        response.prettyPrint();
+        bookingId = response.jsonPath().getInt("bookingid");
+
 
         BookingResponsePojo actualData = response.as(BookingResponsePojo.class);
         assertEquals(200, response.statusCode());
-        assertEquals(payLoad.getFirstname(),actualData.getBooking().getFirstname());
-        assertEquals(payLoad.getLastname(),actualData.getBooking().getLastname());
-        assertEquals(payLoad.getTotalprice(),actualData.getBooking().getTotalprice());
-        assertEquals(payLoad.getDepositpaid(),actualData.getBooking().getDepositpaid());
-        assertEquals(payLoad.getAdditionalneeds(),actualData.getBooking().getAdditionalneeds());
-        assertEquals(bookingDates.getCheckin(),actualData.getBooking().getBookingdates().getCheckin());
-        assertEquals(bookingDates.getCheckout(),actualData.getBooking().getBookingdates().getCheckout());
+        assertEquals(payLoad.getFirstname(), actualData.getBooking().getFirstname());
+        assertEquals(payLoad.getLastname(), actualData.getBooking().getLastname());
+        assertEquals(payLoad.getTotalprice(), actualData.getBooking().getTotalprice());
+        assertEquals(payLoad.getDepositpaid(), actualData.getBooking().getDepositpaid());
+        assertEquals(payLoad.getAdditionalneeds(), actualData.getBooking().getAdditionalneeds());
+        assertEquals(bookingDates.getCheckin(), actualData.getBooking().getBookingdates().getCheckin());
+        assertEquals(bookingDates.getCheckout(), actualData.getBooking().getBookingdates().getCheckout());
 
     }
 }
