b6f1321285d957ee1a7851ccc33e8c236927bafb
buusra <busrakirsac@gmail.com>
2023-12-28 08:02:46 +0300

    v4

--- src/test/java/gmi_bank/PostCountry.java
+++ src/test/java/gmi_bank/PostCountry.java
@@ -1,7 +1,16 @@
 package gmi_bank;
 
 import baseUrl.GmiBankBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
+import pojos.gmibank.CountryPojo;
+import pojos.gmibank.StatesPojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class PostCountry extends GmiBankBaseUrl {
 
@@ -63,7 +72,33 @@ public class PostCountry extends GmiBankBaseUrl {
     @Test
     public void postCountry() {
 
-        spec.pathParams("first","api","second","tp-countries");
+        spec.pathParams("first", "api", "second", "tp-countries");
+
+        StatesPojo states1 = new StatesPojo(1, "Elma");
+        StatesPojo states2 = new StatesPojo(2, "Armut");
+        StatesPojo states3 = new StatesPojo(3, "Portakal");
+
+        List<StatesPojo> statesList = new ArrayList<>();
+        statesList.add(states1);
+        statesList.add(states2);
+        statesList.add(states3);
+
+
+        CountryPojo payLoad = new CountryPojo("Muz Cumhuriyeti", statesList);
+
+        Response response = given(spec).body(payLoad).when().post("{first}/{second}");
+        response.prettyPrint();
+
+
+        CountryPojo actualData = response.as(CountryPojo.class);
+        assertEquals(payLoad.getName(), actualData.getName());
+        assertEquals(states1.getId(), actualData.getStates().get(0).getId());
+        assertEquals(states1.getName(), actualData.getStates().get(0).getName());
+        assertEquals(states2.getId(), actualData.getStates().get(1).getId());
+        assertEquals(states2.getName(), actualData.getStates().get(1).getName());
+        assertEquals(states3.getId(), actualData.getStates().get(2).getId());
+        assertEquals(states3.getName(), actualData.getStates().get(2).getName());
+
 
 
     }
